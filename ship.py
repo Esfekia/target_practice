44dfb44105a51ff883f1bfab45b8d40aaa8002e0
esfekia <sertac@gmail.com>
2022-01-03 05:50:38 -0500

    Added the ship module and main code init.

---
+++ ship.py
@@ -0,0 +1,47 @@
+import pygame
+
+class Ship:
+	"""A Class to manage the ship."""
+
+	def __init__(self, tp_game):
+		"""Initialize the ship and set its starting position."""
+		self.screen = tp_game.screen
+		self.settings = tp_game.settings
+		self.screen_rect = tp_game.screen.get_rect()
+
+		#Load the ship image and get its rect.
+		self.image = pygame.image.load('images/ship.bmp')
+		self.rect = self.image.get_rect()
+
+		#Start each new ship at the middle left of the screen.
+		self.rect.midleft = self.screen_rect.midleft
+
+		#Store a decimal value for the ships x,y position.
+		self.x = float (self.rect.x)
+
+		#Movement flag
+		self.moving_up = False
+		self.moving_down = False
+
+	def update(self):
+		"""Update the ship's position based on the movement flags."""
+
+		#Update the ship's y value, not the rect.
+		if self.moving_up and self.rect.bottom < self.screen_rect.bottom:
+			self.y += self.settings.ship_speed 
+
+		if self.moving_down and self.rect.up > 0:
+			self.y -= self.settings.ship_speed
+
+		#Update the rect object from self.y
+		self.rect.y = self.y
+
+	def center_ship (self):
+		"""Center the ship on midleft of the screen."""
+		self.rect.midleft = self.screen_rect.midleft
+		self.x = float(self.rect.x)
+		self.y =float(self.rect.y)
+
+	def blitme(self):
+		"""Draw the ship at its current location."""
+		self.screen.bli(self.image, self.rect)
--- target_practice.py
+++ target_practice.py
@@ -9,6 +9,7 @@ from bullet import Bullet
 from rectangle import Rectangle
 from game_stats import GameStats
 from button import Button
+from target import Target
 
 class TargetPractice:
 	"""Overall class to manage game assets and behavior."""
@@ -20,4 +21,19 @@ class TargetPractice:
 
 		self.settings = Settings()
 		self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-		self.settings.screen_width = self.
+		self.settings.screen_width = self.screen.get_rect().width
+		self.screen.screen_height = self.screen.get_rect().height
+		pygame.display.set_caption("Target Practice")
+
+		#Create an instance to store the game statistics.
+		self.stats = GameStats(self)
+		self.ship = Ship(self)
+		self.bullets = pygame.sprite.Group()
+		
+		#Create the rectangular, moving target.
+		self._create_target()
+
+		#Create the Play button.
+		self.play_button = Button(self, "Play") 
+
+
